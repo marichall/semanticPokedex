9e9bba1b574f0adaa8c77f3ff989eec591fddba6
Hanybay <hanybayazid11@gmail.com>
2024-12-30 19:28:31 +0100

    ADD : Ajout d'un bean Rest, qui permet d'effectuer des requêtes vers l'API WikiMedia

--- pokedex/src/main/java/semantic/pokedex/PokedexApplication.java
+++ pokedex/src/main/java/semantic/pokedex/PokedexApplication.java
@@ -2,6 +2,8 @@ package semantic.pokedex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -10,4 +12,8 @@ public class PokedexApplication {
 		SpringApplication.run(PokedexApplication.class, args);
 	}
 
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
